91e5839cbdbe1b2e1e69eb9a9016b6a46abe8088
Unknown <alex_m6m@hotmail.com>
2018-09-08 09:15:23 -0500

    Input Form component
    Input Form showing values on submit

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import InputForm from './components/InputForm.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <div id="title">
+            <span>Calendar</span>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <InputForm /> 
+        </main>
       </div>
     );
   }
---
+++ calendar/src/components/InputForm.js
@@ -0,0 +1,103 @@
+import React from "react";
+
+
+
+class InputForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        dateStart: new Date(),
+        nDays: 1,
+        countryCode: "US",
+        render: false
+      };
+  
+      this.handleDateChange = this.handleDateChange.bind(this);
+      this.handleDaysChange = this.handleDaysChange.bind(this);
+      this.handleCodeChange = this.handleCodeChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleDaysChange(event) {
+      this.setState({nDays: event.target.value, render: false});
+    }
+
+    handleDateChange(event) {
+      this.setState({dateStart: event.target.value, render: false});
+    }
+
+    handleCodeChange(event) {
+      this.setState({countryCode: event.target.value, render: false});
+    }
+  
+    handleSubmit(event) {
+      this.setState({render: true});
+      
+      event.preventDefault();
+    }
+  
+    render() {
+      if(this.state.render){
+        return (
+          <div>
+            <form onSubmit={this.handleSubmit}>
+            <center>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>Start Date:</td>
+                        <td><input type="date" value={this.state.dateStart} onChange={this.handleDateChange} /></td>
+                    </tr>
+                    <tr>
+                        <td>Number of Days:</td>
+                        <td><input type="number" value={this.state.nDays} onChange={this.handleDaysChange} /></td>
+                    </tr>
+                    <tr>
+                        <td>Country Code:</td>
+                        <td><input type="text" value={this.state.countryCode} onChange={this.handleCodeChange} /></td>
+                    </tr>
+                    </tbody>
+                </table>
+                </center>
+                <input type="submit" value="Render Calendar" />
+            </form>
+            <div>
+              <p>{this.state.dateStart}</p>
+              <p>{this.state.nDays}</p>
+              <p>{this.state.countryCode}</p>
+            </div>
+          </div>
+        );
+      }
+      else{
+        return (
+            <div>
+            <form onSubmit={this.handleSubmit}>
+                <center>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>Start Date:</td>
+                        <td><input type="date" value={this.state.dateStart} onChange={this.handleDateChange} /></td>
+                    </tr>
+                    <tr>
+                        <td>Number of Days:</td>
+                        <td><input type="number" value={this.state.nDays} onChange={this.handleDaysChange} /></td>
+                    </tr>
+                    <tr>
+                        <td>Country Code:</td>
+                        <td><input type="text" value={this.state.countryCode} onChange={this.handleCodeChange} /></td>
+                    </tr>
+                    </tbody>
+                </table>
+                </center>
+                <input  type="submit" value="Render Calendar" />
+            </form>
+          </div>
+        );
+      }
+      
+    }
+  }
+
+  export default InputForm;
